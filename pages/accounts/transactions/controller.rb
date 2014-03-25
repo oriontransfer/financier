bc892c1386976f761eb5f7a926d5f1f7a91466d6
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2014-03-25 16:23:17 +1300

    Ability to create individual transactions (other than importing data).

--- pages/accounts/transactions/controller.rb
+++ pages/accounts/transactions/controller.rb
@@ -1,6 +1,19 @@
 
 include Direct
 
+def on_new(path, request)
+	@transaction = Financier::Account::Transaction.create(Financier::DB)
+	@transaction.assign(:account => request[:account_id])
+	
+	if request.post?
+		@transaction.assign(request.params)
+		
+		@transaction.save
+		
+		redirect! request[:_return] || "../show?id=#{@transaction.account.id}"
+	end
+end
+
 def on_edit(path, request)
 	@transaction = Financier::Account::Transaction.fetch(Financier::DB, request[:id])
 	
