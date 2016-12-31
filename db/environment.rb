521f9c3569cbf0961e05bb721e9b79e2285ae46b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2017-01-01 00:51:24 +1300

    Fix database connection setup

--- db/environment.rb
+++ db/environment.rb
@@ -1,6 +1,8 @@
 
 DATABASE_ENV = (ENV['DATABASE_ENV'] || RACK_ENV || :development).to_sym
 
+require 'relaxo'
+
 module Financier
 	# Configure the database connection:
 	DB = Relaxo.connect(DATABASE_ENV)
