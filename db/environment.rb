97a1f95836df8494f73dae806653c5a2e064a1d0
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2017-02-13 01:30:15 +1300

    Fix database path in production.

--- db/environment.rb
+++ db/environment.rb
@@ -4,6 +4,8 @@ DATABASE_ENV = (ENV['DATABASE_ENV'] || RACK_ENV || :development).to_s
 require 'relaxo'
 
 module Financier
+	DATABASE_PATH = File.join(__dir__, DATABASE_ENV)
+	
 	# Configure the database connection:
-	DB = Relaxo.connect(DATABASE_ENV, logger: Logger.new($stderr))
+	DB = Relaxo.connect(DATABASE_PATH, logger: Logger.new($stderr))
 end
