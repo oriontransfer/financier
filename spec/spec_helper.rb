f7f9d760856d8b09fef68e49d80bbb0353c49aee
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2018-01-26 19:22:40 +1300

    Initial support for travis/codeclimate.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,6 @@ if ENV['COVERAGE']
 		SimpleCov.start do
 			add_filter "/spec/"
 		end
-		
-		if ENV['TRAVIS']
-			require 'coveralls'
-			Coveralls.wear!
-		end
 	rescue LoadError
 		warn "Could not load simplecov: #{$!}"
 	end
