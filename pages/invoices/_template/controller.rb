9424aee0d6210047e7e938d20959ff1e502247bc
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-10-06 14:44:09 +1300

    Fix mass assignment for customer & company details.

--- pages/invoices/_template/controller.rb
+++ pages/invoices/_template/controller.rb
@@ -1,12 +1,17 @@
 
 prepend Actions
 
+PARAMETERS = {
+	customer: true,
+	company: true,
+}
+
 on 'company' do |request, path|
 	@invoice = Financier::Invoice.create(Financier::DB.current)
-	@invoice.assign(request.params)
+	@invoice.assign(request.params, PARAMETERS)
 end
 
 on 'customer' do |request, path|
 	@invoice = Financier::Invoice.create(Financier::DB.current)
-	@invoice.assign(request.params)
+	@invoice.assign(request.params, PARAMETERS)
 end
